789ce4459edd5c3fdb3b12c1db3b33f5ffe5aa83
pchchv <ipchchv@gmail.com>
2022-10-17 12:01:36 +0300

    Implement API

---
+++ server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/pchchv/ots/server/goredis"
+)
+
+type SECRET struct {
+	KEY   string `json:"key" binding:"required"`
+	VALUE string `json:"value" binding:"required"`
+}
+
+func main() {
+	r := gin.Default()
+	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
+	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+	api := r.Group("/api")
+	goredis.InitPool()
+	// Create secret with key and value in redis
+	api.POST("/secret/v1/set", func(c *gin.Context) {
+		var secret SECRET
+		c.BindJSON(&secret)
+		key, value, err := goredis.Set(secret.KEY, secret.VALUE)
+		if err == nil {
+			fmt.Println(err)
+			c.JSON(200, gin.H{
+				"key":   key,
+				"value": value,
+			})
+		} else {
+			c.JSON(400, gin.H{
+				"message": "failed to save record.",
+			})
+		}
+	})
+	// Get secret with key from redis
+	api.POST("/secret/v1/get", func(c *gin.Context) {
+		var secret SECRET
+		c.BindJSON(&secret)
+		key, value, err := goredis.Get(secret.KEY)
+		if err == nil {
+			fmt.Println(err)
+			c.JSON(200, gin.H{
+				"key":   key,
+				"value": value,
+			})
+		} else {
+			c.JSON(400, gin.H{
+				"message": "failed to get record.",
+			})
+		}
+	})
+	// Simple monitor API to check if API is working
+	api.GET("/monitor", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Status": "READY",
+		})
+	})
+	// Specifying on which port to run
+	r.Run(":8081")
+}
